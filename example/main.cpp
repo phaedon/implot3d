af29f3389330118459e523f85166036f93d821fb
Fædon Jóhannes <phaedonsinis@gmail.com>
2025-02-12 12:31:16 +0000

    Fix example paths and include glad for easier compilation

--- example/main.cpp
+++ example/main.cpp
@@ -6,9 +6,9 @@
 //--------------------------------------------------
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "imgui/backends/imgui_impl_glfw.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
+#include "imgui/imgui.h"
 #include "implot.h"
 #include "implot3d.h"
 #include <iostream>
